d09e52b93744a65807940d6aaeaa1d4ae96cafdf
Panwar <harsh.panwar@capgemini.com>
2021-05-13 23:54:00 +0530

    My Commit 0n 13th Night

--- src/main/java/com/harsh/cropinfodemo1/CropInfoDemo1Application.java
+++ src/main/java/com/harsh/cropinfodemo1/CropInfoDemo1Application.java
@@ -1,15 +1,31 @@
 package com.harsh.cropinfodemo1;
 
+import io.swagger.annotations.Contact;
+import io.swagger.models.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.xmlpull.v1.XmlPullParserException;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.io.FileReader;
+import java.io.IOException;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
 public class CropInfoDemo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CropInfoDemo1Application.class, args);
 	}
+
 
 }
